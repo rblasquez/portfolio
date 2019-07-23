315117b820f8293f120a01d63d575667d51f6b19
Rosmar Blasquez <reblasquez@gmail.com>
2019-07-23 11:26:12 -0400

    Estructura del layout principal

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <article class="resume-wrapper text-center position-relative">
+    <div class="resume-wrapper-inner mx-auto text-left bg-white shadow-lg">
+    <Header></Header>
+    <div id="app">
+      <!--<div id="nav">-->
+        <!--<router-link to="/">Home</router-link> |-->
+        <!--<router-link to="/about">About</router-link>-->
+      <!--</div>-->
+      <router-view/>
     </div>
-    <router-view/>
-  </div>
+
+    </div>
+  </article>
 </template>
+
+<script>
+  import Header from '@/components/header/Header.vue';
+  export default {
+    components: {
+      Header,
+    },
+  };
+</script>
 
 <style>
 
